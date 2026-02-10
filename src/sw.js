bccdd3241521e010f8413e0ecddd717cf9d6fca9
test <test@a.com>
2026-02-10 21:19:42 +0800

    增加wpa模式

---
+++ src/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'math-v1';
+const ASSETS = [
+    '/',
+    'index.html',
+    'math20.html',
+    'mainfest.json',
+    'img/math.png'
+    // 如果你有单独的 css 或 js，也写在这里
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
